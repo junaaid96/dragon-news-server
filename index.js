ad3dcfe28588f421fd2dfbd4a2486696b24bed57
junaaid96 <info.junaidulislam@gmail.com>
2022-10-26 21:37:15 +0600

    ...

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ app.get('/news', (req, res) => {
     res.send(news);
 });
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(singleNews => singleNews._id === id);
+
+    res.send(selectedNews);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    
+    if (id === '08') {
+        res.send(news);
+    } else {
+        const selectedCategory = news.filter(singleCategory => singleCategory.category_id === id);
+        res.send(selectedCategory);
+    }
+
+});
+
 app.listen(port, () => {
     console.log(`server running at ${port}`);
 });
