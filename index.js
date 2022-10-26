2901d676fdb779ec2eeac053e6356ffd6b65a369
junaaid96 <info.junaidulislam@gmail.com>
2022-10-26 20:48:27 +0600

    ...

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API running');
@@ -13,7 +14,11 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
-})
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
 
 app.listen(port, () => {
     console.log(`server running at ${port}`);
